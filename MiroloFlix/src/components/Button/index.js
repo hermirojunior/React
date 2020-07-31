3708c1e417e294c1a1d9c8b7d91da2a70b8fbd51
hermiro <hermirofsjr@gmail.com>
2020-07-31 00:20:11 -0300

    Projeto MiroloFlix
    Arquivos do Projeto MiroloFlix (Todo mundo odeia o Flix)

---
+++ MiroloFlix/src/components/Button/index.js
@@ -0,0 +1,26 @@
+﻿import styled from "styled-components";
+
+const Button = styled.a`
+	color: var(--white);
+	background-color: rgb(22, 80, 162);
+	box-shadow: 0 4px 8px 0 rgb(255, 255, 255), 0 4px 20px 0 rgba(255, 255, 255, 0.19);
+	border: 1px solid var(--white);
+	box-sizing: border-box;
+	cursor: pointer;
+	padding: 16px 24px;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 16px;
+	outline: none;
+	border-radius: 5px;
+	text-decoration: none;
+	display: inline-block;
+	transition: opacity .3s;
+
+ 	&:hover,
+ 	&ButtonLink:focus {
+	opacity: .5;
+ }
+`;
+
+export default Button;
---
+++ MiroloFlix/src/components/Menu/index.js
@@ -0,0 +1,21 @@
+﻿//Criação do Componente referente ao MENU do APP
+import React from 'react';
+import Logo from '../../assets/images/logo_TMOF.png'
+import '../Menu/Menu.css';
+//import ButtonLink from '../components/ButtonLink';
+import Button from '../Button'
+
+function Menu() {
+	return (
+		<nav className="Menu">
+			<a href="/">
+				<img src={Logo} className="Logo"  alt="Todo Mundo Odeia o Flix"/>
+			</a>
+			<Button ahref="/" className="ButtonLink">
+				Novo Vídeo
+			</Button>
+		</nav>
+	);
+}
+
+export default Menu;
